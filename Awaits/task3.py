9c15cd81dd77e221640ad61eea397d6338475757
Inferno-kN <aldosovich@bk.ru>
2025-07-09 16:29:48 +0300

    fix trash

--- Awaits/task3.py
+++ Awaits/task3.py
@@ -1,18 +1,10 @@
 import asyncio
 
-async def coro1(task1, delay):
+async def coro(task, delay):
     await asyncio.sleep(delay)
-    print(task1)
-
-async def coro2(task2, delay):
-    await asyncio.sleep(delay)
-    print(task2)
-
-async def coro3(task3, delay):
-    await asyncio.sleep(delay)
-    print(task3)
+    print(f'Задача {task} решена!')
 
 async def start():
-    await asyncio.gather(coro1('Задача 1', 1), coro2('Задача 2', 2), coro3('Задача 3', 3))
+    await asyncio.gather(coro(1, 1), coro(2, 2), coro(3, 3))
 
 asyncio.run(start())
